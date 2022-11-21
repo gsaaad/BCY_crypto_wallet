2ef7a77f57d0a21ef637a1ecb180378f359a46e2
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-11-21 15:35:57 -0500

    configured login and register to connect to backend and insert user/ check for email and password credentials

--- miwallet/wallet/register.py
+++ miwallet/wallet/register.py
@@ -4,5 +4,7 @@ class registerForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
     # user_name = forms.CharField(max_length=12)
+    dob = forms.DateField()
     email = forms.EmailField()
+
     password = forms.CharField()
--- miwallet/wallet/views.py
+++ miwallet/wallet/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import path
+from django.contrib.auth.models import auth
 from .login import loginForm
 from .register import registerForm
 from .payment import checkAddressForm
@@ -33,19 +34,31 @@ def login(request):
         form = loginForm(request.POST)
         
         if form.is_valid():
-            print("VALIDATED!")
-            print("Email: "+form.cleaned_data['email'])
-            print("Password: "+form.cleaned_data['password'])
+            print("FORM VALIDATED!")
+            form_email =form.cleaned_data['email']
+            form_password = form.cleaned_data['password']
+            print("Email: "+form_email)
+            print("Password: "+form_password)
             db = client['miWallets']
             Users = db['Users']
-            print(form.cleaned_data['email'])
+            print(form_email)
             
-            result = Users.find_one({"email":form.cleaned_data['email']})
-            print(result)
+            is_valid_user = Users.find_one({"email":form_email})
+            # print(is_valid_user)
             
             
-            if result:
-                print("we found your email in your mongo database! login! auth!]")
+            if is_valid_user:
+                print("we found your email in your mongo database!")
+                print("checking password...")
+                is_valid_password = is_valid_user['password'] == form_password
+                print("valid password? ", is_valid_password)
+                if is_valid_password:
+                    print("successful login, valid email and password. AUTH!")
+                    print("user {} has been authenticated".format(form_email))
+                    return redirect('/home')
+                    
+                else:
+                    print("invalid password... try again or reset password")
                 # router to home!
      
     return render(request, 'login.html', {'form':form})
@@ -61,11 +74,17 @@ def register(request):
         
         if form.is_valid():
             print("VALIDATED!")
-            print("First_name"+ form.cleaned_data['first_name'])
-            print("last_name"+ form.cleaned_data['last_name'])
-            print("user_name"+ form.cleaned_data['user_name'])
-            print("Email: "+form.cleaned_data['email'])
-            print("Password: "+form.cleaned_data['password'])
+            form_firstname = form.cleaned_data['first_name']
+            form_lastname = form.cleaned_data['last_name']
+            form_email = form.cleaned_data['email']
+            form_password = form.cleaned_data['password']
+            form_dob = str(form.cleaned_data['dob'])
+            print("First_name"+ form_firstname)
+            print("last_name"+ form_lastname)
+            # print("user_name"+ form.cleaned_data['user_name'])
+            print("Email: "+form_email)
+            print("date of birth: ",form_dob)
+            print("Password: "+form_password)
             db = client['miWallets']
             Users = db['Users']
             
@@ -73,18 +92,20 @@ def register(request):
             
             if result:
                 #we have this email in our record.. did you mean to login?
-                print("We found this email in our records.. Did you mean to login")
+                print("We found this email in our records.. Did you mean to login?")
             else:
                 # not found in database, create new
-                user = {"first_name": form.cleaned_data['first_name'], 
-                    "last_name": form.cleaned_data['last_name'], 
-                    "user_name": form.cleaned_data['user_name'],
-                    'email': form.cleaned_data['email'],
-                    'password': form.cleaned_data['password']
+                user = {"first_name": form_firstname, 
+                    "last_name": form_lastname, 
+                    'email': form_email,
+                    'password': form_password,
+                    'dob': (form_dob),
+                    'Wallet': []
                     #todo give them a wallet ID, based on database?
                     }
                 Users.insert_one(user)
                 print("REGISTRATION COMPLETE! WELCOME TO MIWALLET")
+                return redirect('/home')
                 
     return render(request, 'register.html', {'form': form})
 
@@ -98,7 +119,6 @@ def payment(request):
             print("validated")
             address = form.cleaned_data["address"]
             is_valid_address = check_valid_address(address)
-            is_valid_address_for_coinsymbol = block
             
             
             
